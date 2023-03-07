bdaf4ffb4fa1c35043ec663e3ee8a1216764a75c
Kwesi Rutledge <kwesir@mit.edu>
2023-03-07 14:19:14 -0500

    Forgot to Add Init File

--- neural_clbf/systems/adaptive/__init__.py
+++ neural_clbf/systems/adaptive/__init__.py
@@ -5,6 +5,7 @@ from .load_sharing_manipulator import LoadSharingManipulator
 from .scalar_demo_capa2_system import ScalarCAPA2Demo
 from .tumbling_target import TumblingTarget
 from .tumbling_target2 import TumblingTarget2
+from .pusher_slider_force_input import PusherSliderStickingForceInput
 
 __all__ = [
     "ControlAffineParameterAffineSystem",
@@ -12,6 +13,7 @@ __all__ = [
     "ScalarCAPA2Demo",
     "TumblingTarget",
     "TumblingTarget2",
+    "PusherSliderStickingForceInput",
 ]
 
 # try:
