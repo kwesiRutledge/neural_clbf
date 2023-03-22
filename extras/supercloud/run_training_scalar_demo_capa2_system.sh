2c2a7a60cb3fc0afcbcba2f30ea202a36ae93558
Kwesi Rutledge <kwesir@mit.edu>
2023-03-22 11:51:12 -0400

    Moved some files into a new supercloud directory; think I'm ready to train on Supercloud using batch jobs

---
+++ extras/supercloud/run_training_scalar_demo_capa2_system.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J USERNAME
+#SBATCH -o %j.stdout
+#SBATCH -e %j.stderr
+#SBATCH -c 20
+#SBATCH --gres=gpu:volta:4
+#SBATCH --time=24:00:00
+
+# Write your commands here
+
+# Load Anaconda
+module load anaconda/2023a
+
+# Activate Environment
+conda activate neural_clbf
+
+# Enter Training Directory for scalar_demo_capa2_system
+cd /home/gridsan/krutledge/neural_clbf/neural_clbf/training/adaptive/
+python train_scalar_demo_capa2_system.py $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
