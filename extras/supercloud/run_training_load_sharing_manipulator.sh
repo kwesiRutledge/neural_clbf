9ddb1b233e47ae84851367fa20f16971cbf23869
Kwesi Rutledge <kwesir@mit.edu>
2023-04-22 17:15:52 -0400

    Updated GPU request to be 2 and removed oracle_loss

--- extras/supercloud/run_training_load_sharing_manipulator.sh
+++ extras/supercloud/run_training_load_sharing_manipulator.sh
@@ -4,8 +4,8 @@
 #SBATCH -o outputs/loaded-manipulator-%j.stdout
 #SBATCH -e outputs/loaded-manipulator-%j.stderr
 #SBATCH -c 40
-#SBATCH --gres=gpu:volta:4
-#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:volta:2
+#SBATCH --time=30:00:00
 
 # Write your commands here
 
@@ -18,7 +18,7 @@ source activate neural_clbf
 # Enter Training Directory for scalar_demo_capa2_system
 cd /home/gridsan/krutledge/neural_clbf/neural_clbf/training/adaptive/
 python train_load_sharing_manipulator.py \
-  --max_epochs 201 --clf_lambda 0.4 \
-  --num_cpu_cores 20 --number_of_gpus 4 \
-  --include_oracle_loss True --barrier True \
+  --max_epochs 151 --clf_lambda 1.0 \
+  --num_cpu_cores 20 --number_of_gpus 2 \
+  --include_oracle_loss False --barrier True \
   --gradient_clip_val 10000.0
