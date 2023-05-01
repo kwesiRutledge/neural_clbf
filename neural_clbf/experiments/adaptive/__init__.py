4dac8a9f48698474997f69c8a48962b85cb0058d
Kwesi Rutledge <kwesir@mit.edu>
2023-05-01 12:27:48 -0400

    Moved problematic case study experiment

--- neural_clbf/experiments/adaptive/__init__.py
+++ neural_clbf/experiments/adaptive/__init__.py
@@ -2,7 +2,7 @@ from .aclf_contour_experiment import AdaptiveCLFContourExperiment
 from .aclf_countour_state_slice_experiment import aCLFCountourExperiment_StateSlices
 from .rollout_manipulator_convergence_experiment import RolloutManipulatorConvergenceExperiment
 from .rollout_parameter_convergence_experiment import RolloutParameterConvergenceExperiment
-from .case_study_safety_experiment import CaseStudySafetyExperiment
+from neural_clbf.experiments.adaptive.safety_case_study.case_study_safety_experiment import CaseStudySafetyExperiment
 
 __all__ = [
     "AdaptiveCLFContourExperiment",
