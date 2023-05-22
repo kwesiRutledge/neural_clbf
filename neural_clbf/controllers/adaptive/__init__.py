bbc8d7e2f0a379dde0e1e6a7ed7c967dd33d8620
Kwesi Rutledge <kwesir@mit.edu>
2023-05-22 04:41:47 -0400

    Introduced Certainty Equivalent Control Idea

--- neural_clbf/controllers/adaptive/__init__.py
+++ neural_clbf/controllers/adaptive/__init__.py
@@ -1,11 +1,14 @@
 from .aclf_controller import aCLFController
 from .aclf_controller2 import aCLFController2
 from .aclf_controller3 import aCLFController3
+from .aclf_controller4 import aCLFController4
 from .neural_aclbf_controller import NeuralaCLBFController
 from .neural_aclbf_controller2 import NeuralaCLBFController2
 from .neural_aclbf_controller3 import NeuralaCLBFController3
+from .neural_aclbf_controller4 import NeuralaCLBFController4
 
 __all__ = [
-    "aCLFController", "aCLFController2", "aCLFController3",
+    "aCLFController", "aCLFController2", "aCLFController3", "aCLFController4",
     "NeuralaCLBFController", "NeuralaCLBFController2", "NeuralaCLBFController3",
+    "NeuralaCLBFController4",
 ]
