98fb6a3a24e1f97b173a99a7154e000496d532c2
Kwesi Rutledge <kwesir@mit.edu>
2023-05-31 10:19:16 -0700

    Preparing For SuperCloud Training Again

--- extras/supercloud/run_training_pusher_slider_force_input_w_scenarios.sh
+++ extras/supercloud/run_training_pusher_slider_force_input_w_scenarios.sh
@@ -19,9 +19,10 @@ source activate neural_clbf
 cd /home/gridsan/krutledge/neural_clbf/neural_clbf/training/adaptive_w_observed_parameters/
 python train_pusher_slider_force_input.py \
   --max_epochs 201 --clf_lambda 0.1 \
-  --safe_level 10.0 \
+  --safe_level 1.0 \
   --num_cpu_cores 20 --number_of_gpus 2 \
   --include_oracle_loss True --barrier True \
   --include_estimation_error_loss False \
+  --learn_shape_epochs 50 --learn_boundary_epochs 50 \
   --gradient_clip_val 10000.0 \
   --max_iters_cvxpylayer 5000000
