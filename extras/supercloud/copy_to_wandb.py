3c8f0d090f03b8f84e29b114167b5479c038fbf7
Kwesi Rutledge <kwesir@mit.edu>
2023-06-04 23:30:30 -0400

    Completed (I think) Python Script that Can Copy and Upload Data to WandB with a simple interface

--- extras/supercloud/copy_to_wandb.py
+++ extras/supercloud/copy_to_wandb.py
@@ -53,7 +53,7 @@ def copy_directory_using_scp(args):
 
 def copy_jobid_stdout_for_processing(args):
     """
-    copy_jobid_stdout_for_processing(args)
+    local_stdout_filename = copy_jobid_stdout_for_processing(args)
     Description:
         Copy jobid stdout for processing
     """
@@ -84,21 +84,80 @@ def copy_jobid_stdout_for_processing(args):
 
     return local_filename
 
-
-def find_logfile_location_on_supercloud(local_stdout_filename):
+def copy_jobid_stderr_for_processing(args):
     """
-    find_logfile_location_on_supercloud(local_stdout_filename)
+    local_stderr_filename = logcopy_jobid_stdout_for_processing(args)
+    Description:
+        Copy jobid stdout for processing
+    """
+
+    # Create source directory name
+    source_directory = "krutledge@txe1-login.mit.edu:~/neural_clbf/extras/supercloud/outputs/"
+    source_filename = ""
+    if args.system == "ps":
+        source_filename += "pusher-slider-"
+    elif args.system == "scalar":
+        source_filename += "scalar-"
+    elif args.system == "ls":
+        source_filename += "loaded-manipulator-"
+    else:
+        raise ValueError("System {} not recognized.".format(args.system))
+
+    # Add Job ID to source name
+    source_filename = source_filename + "{}.stderr".format(args.job_id)
+
+    # Copy file to stdouts directory
+    destination_directory = "./data/stderrs/"
+    return_code = os.system("scp -r {} {}".format(source_directory + source_filename, destination_directory))
+    if return_code != 0:
+        raise RuntimeError("scp command failed.")
+
+    # Create name of file on local machine
+    local_filename = "./data/stderrs/{}".format(source_filename)
+
+    return local_filename
+
+def find_logfile_location_on_supercloud(local_stderr_filename):
+    """
+    find_logfile_location_on_supercloud(local_stderr_filename)
     Description:
         Extracts the location of the logfile on supercloud from the local stdout file.
     """
     # Constants
 
     # Open local file
-    f = os.open(local_stdout_filename, "r")
+    f = open(local_stderr_filename, "r")
     for line in f.readlines():
         if line.startswith("wandb: Find logs at: "):
             return line.split("wandb: Find logs at: ")[1].strip()
 
+    # This part should never be reached
+    raise RuntimeError("Could not find logfile location on supercloud.")
+
+
+def find_labels_for_supercloud_data(local_stdout_filename):
+    """
+    commit, version = find_labels_for_supercloud_data(local_stdout_filename)
+    Description:
+        Extracts the location of the logfile on supercloud from the local stdout file.
+    """
+
+    # Constants
+
+    # Create vars
+    commit = ""
+    version = ""
+
+    # Open local file
+    f = open(local_stdout_filename, "r")
+    for line in f.readlines():
+        if line.startswith("[Neural aCLBF] commit"):
+            commit = line.split("[Neural aCLBF] commit ")[1].strip()
+
+        if line.startswith("[Neural aCLBF] version "):
+            version = line.split("[Neural aCLBF] version ")[1].strip()
+
+    return commit, version
 
 def copy_logfile_from_supercloud(supercloud_logfilename):
     """
@@ -116,9 +175,9 @@ def copy_logfile_from_supercloud(supercloud_logfilename):
     print(supercloud_logfilename)
     print(source_directory)
 
-    # Copy file to wandb directory
-    destination_directory = "./data/wandb/"
-    return_code = os.system("scp -r {} {}".format(source_directory, destination_directory))
+    # Copy directory to wandb directory
+    destination_directory = "./data/wandb/" + offline_run + "/"
+    return_code = os.system("rsync -r {} {}".format(source_directory, destination_directory))
     if return_code != 0:
         raise RuntimeError("scp command failed.")
 
@@ -130,7 +189,7 @@ def logfile_declared_name_to_offline_run_name(logfile_declared_name):
     """
     return logfile_declared_name.split("/")[-2]
 
-def upload_logfile_to_wandb(supercloud_logfilename):
+def upload_logfile_to_wandb(supercloud_logfilename, commit: str, version: str):
     """
     upload_logfile_to_wandb(supercloud_logfilename)
     Description:
@@ -142,10 +201,49 @@ def upload_logfile_to_wandb(supercloud_logfilename):
         os.getcwd(),
         logfile_declared_name_to_offline_run_name(supercloud_logfilename),
     )
-
-    return_code = os.system("wandb sync {}".format(local_logfile))
+    print("commit_{}_version_{}".format(
+            commit, version,
+        ))
+    return_code = os.system(
+        "wandb sync --project \"Neural aCLBF\" --id commit_{}_version_{} {}".format(
+            commit, version, local_logfile,
+        ))
     if return_code != 0:
         raise RuntimeError("wandb command failed.")
+
+def copy_controller_data_to_training_data_directory(commit: str, version: str):
+    """
+    copy_controller_data_to_training_data_directory(commit: str, version: str)
+    Description:
+        Copies the controller data to the training data directory.
+    """
+    # Constants
+
+    # Create source directory name
+    source_directory = "krutledge@txe1-login.mit.edu:~/neural_clbf/neural_clbf/training/adaptive_w_observed_parameters/logs/"
+    if args.system == "ps":
+        source_directory += "pusher_slider_sticking_force_input_WandB/"
+    else:
+        raise ValueError("System {} not recognized.".format(args.system))
+    source_directory += "commit_{}/version_{}/".format(commit, version)
+
+    # Create target directory on local machine
+    target_directory = "../../neural_clbf/training/adaptive_w_observed_parameters/logs/"
+    if args.system == "ps":
+        target_directory += "pusher_slider_sticking_force_input_WandB/"
+    else:
+        raise ValueError("System {} not recognized.".format(args.system))
+
+    target_directory += "commit_{}/version_{}/".format(commit, version)
+
+    # Create target directory
+    os.makedirs(target_directory, exist_ok=True)
+
+    # Copy directory to local directory
+    return_code = os.system("scp -r {} {}".format(source_directory, target_directory))
+    if return_code != 0:
+        raise RuntimeError("scp command failed.")
+
 
 if __name__ == "__main__":
     # Input Processing
@@ -168,6 +266,30 @@ if __name__ == "__main__":
 
     # Copy data
     add_ssh_key() # Add ssh-keys to identify.
+
+    # Extract files
     local_stdout_filename = copy_jobid_stdout_for_processing(args) # Copy stdout data to local machine.
-    copy_logfile_from_supercloud(local_stdout_filename)
-    upload_logfile_to_wandb(local_stdout_filename)
+    local_stderr_filename = copy_jobid_stderr_for_processing(args)  # Copy stderr data to local machine.
+
+    # Extract logfile data
+
+    commit, version = find_labels_for_supercloud_data(local_stdout_filename)
+    print("Parsed the following commit and version info:")
+    print("Commit: {}".format(commit))
+    print("Version: {}\n".format(version))
+
+    logfile_location_on_supercloud = find_logfile_location_on_supercloud(local_stderr_filename)
+    print("Parsed the following logfile location on supercloud:")
+    print(logfile_location_on_supercloud)
+
+    # Copy logfile
+    copy_logfile_from_supercloud(logfile_location_on_supercloud)
+    print("Copied logfile from supercloud to local machine.\n")
+
+    # Upload logfile
+    upload_logfile_to_wandb(logfile_location_on_supercloud, commit, version)
+    print("Uploaded logfile to wandb.\n")
+
+    # Copy controller data to the training data directory
+    copy_controller_data_to_training_data_directory(commit, version)
+    print("Copied controller data to local machine.\n")
